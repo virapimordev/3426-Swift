3e35ff4f51d98b287cab0af3220f7ba19ab6377d
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-19 11:20:58 +0300

    init

---
+++ ChicknStand/App/ChicknStandApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChicknStandApp.swift
+//  ChicknStand
+//
+//  Created by Serhii Babchuk on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChicknStandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CSLaunchView()
+        }
+    }
+}
---
+++ ChicknStand/Screens/LaunchScreen/CSLaunchView.swift
@@ -0,0 +1,119 @@
+import SwiftUI
+
+struct CSLaunchView: View {
+    
+    @State private var progress: CGFloat = 0.0
+    @State private var isActive = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                loader
+                
+                NavigationLink(
+                    destination: CSHomeWebView(),
+                    isActive: $isActive
+                ) {
+                    EmptyView()
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .frame(maxHeight: .infinity)
+            .background(
+                ZStack {
+                    Color.white
+                    Image(.loadingBackground)
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                }
+            )
+            .onAppear {
+                progress = 0
+                Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { timer in
+                    if progress < 1 {
+                        progress += 0.01
+                    } else {
+                        timer.invalidate()
+                        isActive = true
+                    }
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        .hideNavigationBar()
+    }
+}
+
+// MARK: - Loader
+
+extension CSLaunchView {
+    var loader: some View {
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.white)
+                .frame(height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(
+                            LinearGradient(
+                                colors: [
+                                    .green,
+                                    .green
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            ),
+                            lineWidth: 3
+                        )
+                )
+            
+            RoundedRectangle(cornerRadius: 8)
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .green,
+                            .green
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(
+                            LinearGradient(
+                                colors: [
+                                    .white,
+                                    .white
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            ),
+                            lineWidth: 2
+                        )
+                )
+                .frame(width: progress * 280, height: 35)
+                .animation(.linear(duration: 3), value: progress)
+            
+            HStack {
+                Text("LOADING...")
+                    .foregroundStyle(.yellow2)
+                    .font(.system(size: 16, weight: .bold, design: .default))
+                
+                Text("\(Int(progress * 100))%")
+                    .foregroundStyle(.yellow2)
+                    .font(.system(size: 16, weight: .bold, design: .default))
+            }
+            .padding(.horizontal, 12)
+        }
+        .frame(width: 280)
+    }
+}
+
+#Preview {
+    CSLaunchView()
+}
